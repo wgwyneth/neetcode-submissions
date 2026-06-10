5f27dff6c8469cebd1714ecf9268da14b3edba7a
Gwyneth Wood <114595644+wgwyneth@users.noreply.github.com>
2026-06-10 16:47:10 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    '''One Pass Implementation'''
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashy = {}
+        for i in range(len(nums)):
+            if i != 0:
+                diff = target - nums[i]
+                if diff in hashy:
+                    return [hashy[diff], i]
+            hashy[nums[i]] = i
